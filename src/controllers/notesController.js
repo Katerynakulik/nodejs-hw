79b3766326f1ce6e13e4010f45f1620211619a17
Katerynakulik <katerynakulik4@gmail.com>
2025-11-15 00:50:08 +0200

    Add search in database

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,16 +2,24 @@ import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
 
 export const getAllNotes = async (req, res) => {
-  const { page = 1, perPage = 10 } = req.query;
+  const { page = 1, perPage = 10, tag, search } = req.query;
   console.log(page, perPage);
   const skip = (page - 1) * perPage;
-  const limit = perPage;
 
   const notesQuery = Note.find();
+  if (search) {
+    notesQuery.where({
+      $text: { $search: search },
+    });
+  }
+
+  if (tag) {
+    notesQuery.where('tag').equals(tag);
+  }
 
   const [totalNotes, notes] = await Promise.all([
     notesQuery.clone().countDocuments(),
-    notesQuery.skip(skip).limit(limit),
+    notesQuery.skip(skip).limit(perPage),
   ]);
   const totalPages = Math.ceil(totalNotes / perPage);
 
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,5 +1,6 @@
 import { Joi, Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const objectIdValidator = (value, helpers) => {
   if (!isValidObjectId(value)) {
@@ -12,25 +13,16 @@ export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(...TAGS),
+    search: Joi.string().trim().allow(''),
   }),
 };
 
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
-    content: Joi.string().min(0),
-    tag: Joi.string().valid(
-      'Work',
-      'Personal',
-      'Meeting',
-      'Shopping',
-      'Ideas',
-      'Travel',
-      'Finance',
-      'Health',
-      'Important',
-      'Todo',
-    ),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
   }),
 };
 
@@ -45,17 +37,6 @@ export const updateNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1),
     content: Joi.string().min(0),
-    tag: Joi.string().valid(
-      'Work',
-      'Personal',
-      'Meeting',
-      'Shopping',
-      'Ideas',
-      'Travel',
-      'Finance',
-      'Health',
-      'Important',
-      'Todo',
-    ),
+    tag: Joi.string().valid(...TAGS),
   }).min(1),
 };
